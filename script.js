8ab7cc42851a1b8d9acdf3c007d37f155474e0cd
rehla55day-del <rehla55day@gmail.com>
2026-03-08 02:19:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -678,11 +678,4 @@
 
     loadLeaderboard();
 })();
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/firebase-messaging-sw.js')
-    .then((registration) => {
-      console.log('Service Worker registered with scope:', registration.scope);
-    }).catch((err) => {
-      console.log('Service Worker registration failed:', err);
-    });
-}
+navigator.serviceWorker.register('firebase-messaging-sw.js') // شلنا الـ / اللي في الأول
